7ea0ab414ddb08cd0c4e34bf0f7a31cb41f291a8
Gabriel Almeida Rizzutto <garizzutto@gmail.com>
2022-11-17 19:32:47 -0300

    feat: testing sub-controller

--- server/index.js
+++ server/index.js
@@ -1,15 +1,7 @@
-import Express, { json } from 'express';
-const app = Express();
-
-import cors from 'cors';
-import router from './router.js';
+import app from './server'
 
 const PORT = process.env.PORT;
 
-
-app.use(cors());
-app.use(json());
-app.use(router);
-
-
-app.listen(PORT, () => console.log(`Listening on port ${PORT}! 🚀`));
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}! 🚀`)
+});
---
+++ server/tests/mocks.js
@@ -0,0 +1,11 @@
+export default {
+  subscriptionBody: {
+    icon: 'a',
+    price: 10,
+    title: 'Mock Title',
+    start: new Date(),
+    prettyStart: new Date().toISOString(),
+    cycle: 'Monthly',
+    reminderDate: new Date(),
+  }
+}

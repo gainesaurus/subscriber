eb0cc6a4176977e5f4d195f6d5633e0055cbd0d5
Gabriel Almeida Rizzutto <garizzutto@gmail.com>
2022-11-17 22:12:55 -0300

    feat: sub-list refactor

---
+++ client/src/components/sub-list.tsx
@@ -0,0 +1,16 @@
+import './sub-list.css';
+import SubItem from './sub-item';
+import React from 'react';
+import { Subscription } from '../types';
+
+function SubscriptionList({ subscriptions }: { subscriptions: Array<Subscription> }) {
+
+  return (<div className='list-cont'>
+    {subscriptions && subscriptions.map((sub) =>
+      <SubItem key={sub._id} subscription={sub} />
+    )}
+
+  </div>);
+}
+
+export default SubscriptionList;

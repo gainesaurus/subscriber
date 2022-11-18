8f82c8512055eb950ce241074e5a048caf5beacd
gainey.michael <gainey.michael@gmail.com>
2022-11-18 15:55:07 -0800

    switching to up-to-date branch

--- client/src/tests/api-service.test.js
+++ client/src/tests/api-service.test.js
@@ -10,7 +10,18 @@ import {
 
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import { it } from 'node:test';
 
 jest.mock('../api-service/api-ServiceWorker.js', () => ({
-  getAllSubs: ()
+  getAllSubs: () => ({})
+}));
+it('should call getAllSubs with the correct credentials', async() => {
+  const setAllSubs = jest.fn();
+  const credentials = {};
+
+  // render(<)
 })
+
+jest.mock('../api-service/api-ServiceWorker.js', () => ({
+
+}))
--- client/src/tests/sub-item.test.js
+++ client/src/tests/sub-item.test.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import '@testing-library/jest-dom';
 import { render, screen, fireEvent } from '@testing-library/react';
+import {BrowserRouter as Router} from 'react-router-dom';
 import SubItem from '../components/sub-item';
-import { mocks, mockSubItem } from './mocks';
+import { mockSubItem } from './mocks';
 
 let mockFunction;
 
@@ -12,31 +13,33 @@ beforeEach(() => {
 
 describe('SubItem Tests', () => {
 
-  it('Should render the Subscription Item title correctly', () => {
+  it('Should render the SubItem title correctly', () => {
     render(
-      <SubItem key={mockSubItem._id} subscription={mockSubItem} />
+      <Router>
+        <SubItem key={mockSubItem._id} subscription={mockSubItem} />
+      </Router>
     );
-    expect(screen.getByText(mocks.mockSubItem.title)).toBeInTheDocument();
+    expect(screen.getByText(mockSubItem[0].title)).toBeInTheDocument();
   })
 
-  it('Should render the Subscription Item prettyStart correctly', () => {
+  it('Should render the SubItem prettyStart correctly', () => {
     render(
       <SubItem key={mockSubItem._id} subscription={mockSubItem} />
     );
-    expect(screen.getByText(mocks.mockSubItem.prettyStart)).toBeInTheDocument();
+    expect(screen.getByText(mockSubItem[0].prettyStart)).toBeInTheDocument();
   })
 
-  it('Should render the Subscription Item price correctly', () => {
+  it('Should render the SubItem price correctly', () => {
     render(
       <SubItem key={mockSubItem._id} subscription={mockSubItem} />
     );
-    expect(screen.getByText(mocks.mockSubItem.price)).toBeInTheDocument();
+    expect(screen.getByText(mockSubItem.price)).toBeInTheDocument();
   })
 
-  it('Should render the Subscription Item cycle correctly', () => {
+  it('Should render the SubItem cycle correctly', () => {
     render(
-      <SubItem key={mockSubItem._id} subscription={mockSubItem} />
+      <SubItem key={mockSubItem._id} subscription={mockSubItem} />, {wrapper: Router}
     );
-    expect(screen.getByText(mocks.mockSubItem.cycle)).toBeInTheDocument();
+    expect(screen.getByText(mockSubItem[0].cycle)).toBeInTheDocument();
   })
 })

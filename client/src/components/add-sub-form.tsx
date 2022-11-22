3fa429aeee984e9bef562b8a9d5f0291444e65e2
Gabriel Almeida Rizzutto <garizzutto@gmail.com>
2022-11-22 19:25:02 -0300

    feat: tests for the forms components and refactor subform

--- client/src/components/add-sub-form.tsx
+++ client/src/components/add-sub-form.tsx
@@ -2,11 +2,10 @@ import './add-sub-form.css';
 import React from 'react';
 import 'react-datalist-input/dist/styles.css';
 import SubForm from './sub-form';
-import { postNewSub } from '../api-service/api-service';
 
 function AddSubForm() {
   return (<>
-    <SubForm apiServiceMethod={postNewSub} />
+    <SubForm />
   </>);
 }
 
--- client/src/components/sub-form.tsx
+++ client/src/components/sub-form.tsx
@@ -4,40 +4,37 @@ import { Link, useNavigate } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
 import { Notification, Subscription } from '../types';
-import { postSubNotification, deleteSub } from '../api-service/api-service';
+import { postSubNotification, deleteSub, editSub, postNewSub } from '../api-service/api-service';
 import SubFormItem from './sub-form-item';
 
 type Props = {
-  apiServiceMethod: (formData: Subscription) => void;
   subscription?: Subscription
 }
 
-function SubForm({ apiServiceMethod, subscription }: Props) {
+function SubForm({ subscription }: Props) {
 
-  let initialState = {
-    imageURL: './add-image-icon.png',
-    price: 0,
-    title: 'Title here',
-    start: '',
-    cycle: 'Choose Cycle',
-    reminderDate: '',
-  };
-  if (subscription) initialState = {
-    imageURL: subscription.icon,
-    price: subscription.price,
-    title: subscription.title,
-    start: subscription.start,
-    cycle: subscription.cycle,
-    reminderDate: subscription.reminderDate,
-  };
+  let initialState: Subscription;
+  let apiServiceMethod: (formData: Subscription) => void;
+
+  if (subscription) {
+    initialState = subscription;
+    apiServiceMethod = editSub
+  }
+  else {
+    initialState = {
+      icon: './add-image-icon.png',
+      price: 0,
+      title: 'Title here',
+      start: '',
+      cycle: 'Choose Cycle',
+      reminderDate: '',
+      prettyStart: '',
+    };
+    apiServiceMethod = postNewSub
+  }
 
   const [imageFile, setImageFile] = useState('');
-  const [imageURL, setImageURL] = useState(initialState.imageURL);
-  const [price, setPrice] = useState(initialState.price);
-  const [title, setTitle] = useState(initialState.title);
-  const [start, setStart] = useState(initialState.start);
-  const [cycle, setCycle] = useState(initialState.cycle);
-  const [reminderDate, setReminderDate] = useState(initialState.reminderDate);
+  const [sub, setSub] = useState(initialState)
   const navigate = useNavigate();
   const imageUploader = React.useRef<HTMLInputElement>(null);
 
@@ -53,32 +50,31 @@ function SubForm({ apiServiceMethod, subscription }: Props) {
     imgData = await imgData.json();
     return imgData;
   }
-  // END OF HELPER FUNCTIONS
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
 
-    let dateTime = new Date(start);
+    let dateTime = new Date(sub.start);
     const prettyStart = dateTime.toDateString();
     const imgData = await uploadImage();
 
     apiServiceMethod({
       icon: imgData.url,
-      price,
-      title,
-      start,
+      price: sub.price,
+      title: sub.title,
+      start: sub.start,
       prettyStart,
-      cycle,
-      reminderDate: new Date(reminderDate).toISOString(),
+      cycle: sub.cycle,
+      reminderDate: new Date(sub.reminderDate).toISOString(),
       _id: subscription?._id,
     });
 
-    if (reminderDate) {
-      const delay = (new Date().getTime() - new Date(reminderDate).getTime()) / 1000
+    if (sub.reminderDate) {
+      const delay = (new Date().getTime() - new Date(sub.reminderDate).getTime()) / 1000
       const notification: Notification = {
         userId: '1234', //for testing purposes
-        title: title,
-        price: price,
+        title: sub.title,
+        price: sub.price,
         delay: delay,
       }
       postSubNotification(notification)
@@ -100,19 +96,19 @@ function SubForm({ apiServiceMethod, subscription }: Props) {
             accept='image/*'
             multiple={false}
             onChange={(e) => {
-              setImageURL(URL.createObjectURL(e.target.files![0]));
+              setSub({ ...sub, icon: URL.createObjectURL(e.target.files![0]) });
               setImageFile(e.target.files![0].name);
             }}
             ref={imageUploader}
             style={{ display: 'none' }}
           ></input>
           <div className='icon-display' onClick={() => { if (imageUploader.current) { imageUploader.current.click() } }}>
-            <img className='icon' src={imageURL} alt='Add Icon' />
+            <img className='icon' src={sub.icon} alt='Add Icon' />
           </div>
           <SubFormItem
             label='$'
-            data={price}
-            onChange={(e) => setPrice(parseInt(e.target.value))}
+            data={sub.price}
+            onChange={(e) => setSub({ ...sub, price: parseInt(e.target.value) })}
             type='number'
             hasSection={false} />
           <Link to='/'><FontAwesomeIcon icon={faChevronLeft} className='back-btn' /></Link>
@@ -122,30 +118,30 @@ function SubForm({ apiServiceMethod, subscription }: Props) {
         <div className='form-body'>
           <SubFormItem
             label='Title: '
-            data={title}
-            onChange={(e) => setTitle(e.target.value)}
+            data={sub.title}
+            onChange={(e) => setSub({ ...sub, title: e.target.value })}
             type='string' />
           <SubFormItem
             label='First Payment: '
-            data={start}
-            onChange={(e) => setStart(e.target.value)}
+            data={sub.start}
+            onChange={(e) => setSub({ ...sub, start: e.target.value })}
             type='date' />
           <SubFormItem
             label='Cycle: '
-            data={cycle}
-            onChange={(e) => setCycle(e.target.value)}
+            data={sub.cycle}
+            onChange={(e) => setSub({ ...sub, cycle: e.target.value })}
             dataList={true}
             type='text' />
           <SubFormItem
             label='Remind Me: '
-            data={reminderDate}
-            onChange={(e) => setReminderDate(e.target.value)}
+            data={sub.reminderDate}
+            onChange={(e) => setSub({ ...sub, reminderDate: e.target.value })}
             type='datetime-local'
             min={new Date(Date.now()).toISOString()}
           />
           <section>
-            <button className='submit-form-btn' type='button' onClick={() => handleDelete()}>Delete Subscription</button>
-            <button className='submit-form-btn' type='submit' >Edit Subscription</button>
+            {subscription ? <button className='submit-form-btn' type='button' onClick={() => handleDelete()}>Delete Subscription</button> : null}
+            <button className='submit-form-btn' type='submit' >{subscription ? 'Edit' : 'Add'} Subscription</button>
           </section>
         </div>
       </form>

4707a8500f8b05f6ecd99786419b0a4c4f8159d0
gainey.michael <gainey.michael@gmail.com>
2022-11-23 16:58:38 -0800

    feat: add category drop-down to sublist

--- client/src/components/sub-list.tsx
+++ client/src/components/sub-list.tsx
@@ -6,6 +6,18 @@ import { Subscription } from '../types';
 function SubscriptionList({ subscriptions }: { subscriptions?: Subscription[] }) {
 
   return (<div className='list-cont'>
+    <select
+      className='category-filter' name="category"
+      // onChange={(e) => setSub({ ...sub, category: e.target.value })}
+    >
+      <option value="all">all</option>
+      <option value="entertainment">entertainment</option>
+      <option value="education">education</option>
+      <option value="work">work</option>
+      <option value="home">home</option>
+      <option value="food">food</option>
+      <option value="other">other</option>
+    </select>
     {subscriptions && subscriptions.map((sub) =>
       <SubItem key={sub._id} subscription={sub} />
     )}
